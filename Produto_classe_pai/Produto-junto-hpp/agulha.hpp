faeb5811d7928efa640d4e41accebcca06b7970c
João <joaopedronoronhadutra@gmail.com>
2024-06-16 19:19:56 -0300

    Agulha updates

--- Produto_classe_pai/Produto-junto-hpp/agulha.hpp
+++ Produto_classe_pai/Produto-junto-hpp/agulha.hpp
@@ -8,6 +8,7 @@ class Agulha : public Produto{
     public:
         Agulha();
         Agulha(string descricaoprod, int quantidadeprod, float preco);
+        ~Agulha();
         string InformacoesProduto() override;
 
 };
